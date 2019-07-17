f7e56999ffc1785272ae077fd8276c9e67a8b4f1
Leo Brugnara <leobr6@gmail.com>
2019-07-17 17:40:02 -0300

    Remove jmp_buf init

--- src/Defer.h
+++ src/Defer.h
@@ -28,20 +28,20 @@ struct FlDeferScope {
 #define defer_scope_return if (setjmp(scope.exit) == 0) longjmp(scope.call_chain ? scope.call_chain->stmt_entry : scope.exit, 1); else return
 
 #define defer_scope \
-    for (struct FlDeferScope scope = { .call_chain = NULL, .exit = { 0 } }; scope.call_chain == NULL; longjmp(scope.call_chain ? scope.call_chain->stmt_entry : scope.exit, 1)) \
+    for (struct FlDeferScope scope = { .call_chain = NULL, .tmpptr = NULL }; scope.call_chain == NULL; longjmp(scope.call_chain ? scope.call_chain->stmt_entry : scope.exit, 1)) \
         if (setjmp(scope.exit) != 0) \
         { \
             break; \
         } else 
 
 #define defer_statements \
-    scope.call_chain = &(struct FlDeferStmt) { .prev = scope.call_chain, .stmt_entry = { 0 } }; \
+    scope.call_chain = &(struct FlDeferStmt) { .prev = scope.call_chain }; \
     for (struct FlDeferStmt *this = scope.call_chain; \
         setjmp(this->stmt_entry) != 0 ; \
         (scope.call_chain = this->prev, longjmp(scope.call_chain ? scope.call_chain->stmt_entry : scope.exit, 1)))
 
 #define defer_statement(statement) \
-    scope.call_chain = &(struct FlDeferStmt) { .prev = scope.call_chain, .stmt_entry = { 0 } }; \
+    scope.call_chain = &(struct FlDeferStmt) { .prev = scope.call_chain }; \
     if (setjmp(scope.call_chain->stmt_entry) != 0) \
     { \
         statement; \
